9f3e35003de5130d09a6fb289de96a0579e5cdaf
Slowbro213 <thanas.papa.24@gmail.com>
2026-01-12 23:39:42 +0100

    first screen text

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("MyGame")]
 async fn main() {
     loop {
-        clear_background(RED);
+        clear_background(BLACK);
 
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
