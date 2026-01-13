065c8d2c7b327c48750c013dcd60f8046282e566
Slowbro213 <thanas.papa.24@gmail.com>
2026-01-13 19:41:45 +0100

    cloudflare pages test

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("MyGame")]
 async fn main() {
     loop {
-        clear_background(BLACK);
+        clear_background(RED);
 
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
